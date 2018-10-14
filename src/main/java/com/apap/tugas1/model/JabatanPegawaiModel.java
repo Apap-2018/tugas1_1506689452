c4c6455437962f7f8cb480e7d8ae57f08453ad4d
ahmadbaal <asyraf13@hotmail.com>
2018-10-14 20:48:22 +0700

    fitur 1 done

--- src/main/java/com/apap/tugas1/model/JabatanPegawaiModel.java
+++ src/main/java/com/apap/tugas1/model/JabatanPegawaiModel.java
@@ -34,6 +34,15 @@ public class JabatanPegawaiModel implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
+	@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_pegawai",referencedColumnName = "id", nullable = false)
+    @OnDelete(action = OnDeleteAction.NO_ACTION)
+    private PegawaiModel pegawai;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_jabatan",referencedColumnName = "id", nullable = false)
+    @OnDelete(action = OnDeleteAction.NO_ACTION)
+    private JabatanModel jabatan;
 
 	public long getId() {
 		return id;
@@ -41,5 +50,21 @@ public class JabatanPegawaiModel implements Serializable{
 
 	public void setId(long id) {
 		this.id = id;
+	}
+
+	public PegawaiModel getPegawai() {
+		return pegawai;
+	}
+
+	public void setPegawai(PegawaiModel pegawai) {
+		this.pegawai = pegawai;
+	}
+
+	public JabatanModel getJabatan() {
+		return jabatan;
+	}
+
+	public void setJabatan(JabatanModel jabatan) {
+		this.jabatan = jabatan;
 	}
 }
